2e78c90d7da67ea20d3c1abc896dbfee71edfac0
igor <embiggenerd@gmail.com>
2020-02-17 13:06:59 -0500

    get accounts and error mw working

--- server.js
+++ server.js
@@ -6,4 +6,19 @@ const server = express();
 
 server.use(express.json());
 
+server.get('/accounts', async (req, res, next) => {
+    try {
+        const accounts = await db.raw(`SELECT * FROM accounts`)
+        res.json(accounts)
+    }catch(e){
+        next(e)
+    }
+})
+
+server.use((err, req, res, next) => {
+    console.log(err)
+    res.status(err.httpStatusCode || 500).json({
+        message: err.message
+    })
+})
 module.exports = server;
