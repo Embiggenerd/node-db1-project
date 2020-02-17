e983b456b5359b7ec7b88add40b8344e873ff371
igor <embiggenerd@gmail.com>
2020-02-17 16:54:41 -0500

    post /accounts working

--- server.js
+++ server.js
@@ -8,9 +8,28 @@ server.use(express.json());
 
 server.get('/accounts', async (req, res, next) => {
     try {
-        const accounts = await db.raw(`SELECT * FROM accounts`)
+        const accounts = await db.select('*')
+            .from('accounts')
+
         res.json(accounts)
-    }catch(e){
+    } catch (e) {
+        next(e)
+    }
+})
+
+
+server.post('/accounts', async (req, res, next) => {
+    try {
+        const { name, budget } = req.body
+        const newAccount = await db('accounts')
+            .insert({ name, budget })
+        // '.returning' method not available for sqlite
+        const returnedAccount = await db.select('*')
+            .from('accounts')
+            .where({ id: newAccount[0] })
+
+        res.json(returnedAccount)
+    } catch (e) {
         next(e)
     }
 })
